2d8eba74f57ab6fcb7aaf46024bec1fb02fa5e5e
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-13 10:49:25 +0200

    refactor: update syntax to a more modern style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+import './index.css';
 
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
