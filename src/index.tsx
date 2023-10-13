b839b64ffd65bb08b36f7217ddccc9c107d0c3b9
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-13 13:04:47 +0200

    refactor: rename main.tsx for better readability

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import 'normalize.css';
 import './scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
