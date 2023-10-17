f5b3b7087ebc63992357a672166da944df5d0917
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-17 20:42:42 +0200

    refactor: add image exports

---
+++ src/assets/characters/images.ts
@@ -0,0 +1,179 @@
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+import img6 from './6.jpg';
+import img7 from './7.jpg';
+import img8 from './8.jpg';
+import img9 from './9.jpg';
+import img10 from './10.jpg';
+import img11 from './11.jpg';
+import img12 from './12.jpg';
+import img13 from './13.jpg';
+import img14 from './14.jpg';
+import img15 from './15.jpg';
+import img16 from './16.jpg';
+import img17 from './17.jpg';
+import img18 from './18.jpg';
+import img19 from './19.jpg';
+import img20 from './20.jpg';
+import img21 from './21.jpg';
+import img22 from './22.jpg';
+import img23 from './23.jpg';
+import img24 from './24.jpg';
+import img25 from './25.jpg';
+import img26 from './26.jpg';
+import img27 from './27.jpg';
+import img28 from './28.jpg';
+import img29 from './29.jpg';
+import img30 from './30.jpg';
+import img31 from './31.jpg';
+import img32 from './32.jpg';
+import img33 from './33.jpg';
+import img34 from './34.jpg';
+import img35 from './35.jpg';
+import img36 from './36.jpg';
+import img37 from './37.jpg';
+import img38 from './38.jpg';
+import img39 from './39.jpg';
+import img40 from './40.jpg';
+import img41 from './41.jpg';
+import img42 from './42.jpg';
+import img43 from './43.jpg';
+import img44 from './44.jpg';
+import img45 from './45.jpg';
+import img46 from './46.jpg';
+import img47 from './47.jpg';
+import img48 from './48.jpg';
+import img49 from './49.jpg';
+import img50 from './50.jpg';
+import img51 from './51.jpg';
+import img52 from './52.jpg';
+import img53 from './53.jpg';
+import img54 from './54.jpg';
+import img55 from './55.jpg';
+import img56 from './56.jpg';
+import img57 from './57.jpg';
+import img58 from './58.jpg';
+import img59 from './59.jpg';
+import img60 from './60.jpg';
+import img61 from './61.jpg';
+import img62 from './62.jpg';
+import img63 from './63.jpg';
+import img64 from './64.jpg';
+import img65 from './65.jpg';
+import img66 from './66.jpg';
+import img67 from './67.jpg';
+import img68 from './68.jpg';
+import img69 from './69.jpg';
+import img70 from './70.jpg';
+import img71 from './71.jpg';
+import img72 from './72.jpg';
+import img73 from './73.jpg';
+import img74 from './74.jpg';
+import img75 from './75.jpg';
+import img76 from './76.jpg';
+import img77 from './77.jpg';
+import img78 from './78.jpg';
+import img79 from './79.jpg';
+import img80 from './80.jpg';
+import img81 from './81.jpg';
+import img82 from './82.jpg';
+import img83 from './83.jpg';
+import img84 from './84.jpg';
+import img85 from './85.jpg';
+import img86 from './86.jpg';
+import img87 from './87.jpg';
+import img88 from './88.jpg';
+
+export {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40,
+  img41,
+  img42,
+  img43,
+  img44,
+  img45,
+  img46,
+  img47,
+  img48,
+  img49,
+  img50,
+  img51,
+  img52,
+  img53,
+  img54,
+  img55,
+  img56,
+  img57,
+  img58,
+  img59,
+  img60,
+  img61,
+  img62,
+  img63,
+  img64,
+  img65,
+  img66,
+  img67,
+  img68,
+  img69,
+  img70,
+  img71,
+  img72,
+  img73,
+  img74,
+  img75,
+  img76,
+  img77,
+  img78,
+  img79,
+  img80,
+  img81,
+  img82,
+  img83,
+  img84,
+  img85,
+  img86,
+  img87,
+  img88,
+};
