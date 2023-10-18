7444983467bdbd854e218bc0a0e47b6a144499a9
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-18 19:16:45 +0200

    feat: enhance App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Component } from 'react';
 
+import Header from './components/Header/Header';
+import Main from './pages/Main/Main';
+import Footer from './components/Footer/Footer';
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
   }
 }
 
