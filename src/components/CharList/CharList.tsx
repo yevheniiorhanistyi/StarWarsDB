d5cccded69fc3fbef65791acec6a1791ed630865
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-19 21:34:18 +0200

    refactor: remove unsused description

--- src/components/CharList/CharList.tsx
+++ src/components/CharList/CharList.tsx
@@ -29,10 +29,6 @@ const CharList: React.FC<ICharListProps> = ({ data }) => {
           <img className={styles.char__img} src={getCharacterImage(char)} alt={char.name} />
           <div className={styles.char__description}>
             <h4 className={styles.char__title}>{char.name}</h4>
-            <p className={styles.char__attribute}>{`Height: ${char.height}`}</p>
-            <p className={styles.char__attribute}>{`Weigth: ${char.mass}`}</p>
-            <p className={styles.char__attribute}>{`Eye Color: ${char.eye_color}`}</p>
-            <p className={styles.char__attribute}>{`Skin Color: ${char.skin_color}`}</p>
           </div>
         </li>
       ))}
