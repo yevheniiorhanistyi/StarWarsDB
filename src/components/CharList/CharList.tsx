056ddcf46c6d801328f5a8cc02a1f3e1319d69a3
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-02 13:54:35 +0100

    feat: add functionality for open additional info after click on card

--- src/components/CharList/CharList.tsx
+++ src/components/CharList/CharList.tsx
@@ -1,24 +1,22 @@
 import { ICharListProps, ICharData } from '../../types/types';
 
-import * as images from '../../assets/characters/images';
-import emptySrc from '../../assets/fighter.png';
+import { getCharacterImage, getNumberFromString } from '../../utils';
 
 import styles from './CharList.module.scss';
 
-type ImagesType = typeof images;
+const CharList: React.FC<ICharListProps> = ({ data, openInfo }) => {
+  const noResultsImage = '/fighter.png';
 
-const CharList: React.FC<ICharListProps> = ({ data }) => {
-  function getCharacterImage(char: ICharData) {
-    const match = char.url.match(/\d+/);
-    const number = match ? parseInt(match[0], 10) : 1;
-    return images[`img${number}` as keyof ImagesType];
-  }
+  const onOpenInfo = (char: ICharData) => {
+    const number = getNumberFromString(char.url);
+    openInfo(number);
+  };
 
   if (data.length === 0) {
     return (
       <div className={styles.char__empty}>
         <h3>No results found in this galaxy...</h3>
-        <img src={emptySrc} alt="Sormtroopers" />
+        <img src={noResultsImage} alt="Sormtroopers" />
       </div>
     );
   }
@@ -26,10 +24,24 @@ const CharList: React.FC<ICharListProps> = ({ data }) => {
     <ul className={styles.char__list}>
       {data.map((char) => (
         <li key={char.name} className={styles.char__item}>
-          <img className={styles.char__img} src={getCharacterImage(char)} alt={char.name} />
-          <div className={styles.char__description}>
-            <h4 className={styles.char__title}>{char.name}</h4>
-          </div>
+          <button
+            className={styles.char__button}
+            type="button"
+            onClick={() => {
+              onOpenInfo(char);
+            }}
+          >
+            <img className={styles.char__img} src={getCharacterImage(char)} alt={char.name} />
+            <div className={styles.char__description}>
+              <h4 className={styles.char__title}>{char.name}</h4>
+              <p className={styles.char__attribute}>{`Height: ${char.height}`}</p>
+              <p className={styles.char__attribute}>{`Weigth: ${char.mass}`}</p>
+              <p className={styles.char__attribute}>{`Hair Color: ${char.hair_color}`}</p>
+              <p className={styles.char__attribute}>{`Skin Color: ${char.skin_color}`}</p>
+              <p className={styles.char__attribute}>{`Eye Color: ${char.eye_color}`}</p>
+              <p className={styles.char__attribute}>{`Gender: ${char.gender}`}</p>
+            </div>
+          </button>
         </li>
       ))}
     </ul>
