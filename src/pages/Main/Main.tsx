e570923388c4268332bbaff29c8b1f90bd065e58
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-02 13:58:33 +0100

    feat: add functionality for show additional info component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,66 +1,99 @@
-import { useState, useEffect, useCallback, useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import { Outlet } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 
 import { ICharData } from '../../types/types';
-import { getFromLocalStorage, saveToLocalStorage } from '../../utils/localStorage';
+import { getFromLocalStorage, saveToLocalStorage, setContent } from '../../utils';
 
-import ApiService from '../../services/apiService';
-import Search from '../../components/Search/Search';
-import CharList from '../../components/CharList/CharList';
-import Spinner from '../../components/Spinner/Spinner';
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
+import CharList from '../../components/CharList/CharList';
+import SearchInput from '../../components/SearchInput/SearchInput';
+import Pagination from '../../components/Pagination/Pagination';
+
+import useApiService from '../../services/apiService';
 
 import styles from './Main.module.scss';
 
 const Main: React.FC = () => {
-  const [loading, setLoading] = useState(true);
   const [inputValue, setInputValue] = useState(getFromLocalStorage('searchValue') || '');
   const [charList, setCharList] = useState<ICharData[]>([]);
-  const apiService = useMemo(() => new ApiService(), []);
+  const [totalCount, setTotalCount] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const frontPage = searchParams.get('frontpage') || '1';
+  const charID = searchParams.get('details') || '';
 
-  const loadData = useCallback(
-    async (value: string) => {
-      try {
-        setLoading(true);
-        const res = await apiService.getAllCharacters(value);
-        setCharList(res.results);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [setCharList, setLoading, apiService],
-  );
-
-  useEffect(() => {
-    loadData(inputValue);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { getAllCharacters, process, setProcess } = useApiService();
 
   const handleSearchInputChange = (newValue: string) => {
     setInputValue(newValue);
     saveToLocalStorage('searchValue', newValue);
   };
 
-  const handleSubmit = async () => {
-    loadData(inputValue);
+  const openInfo = (charNumber: number) => {
+    setSearchParams(`frontpage=${frontPage}&details=${charNumber}`);
   };
 
-  const mainClass = loading ? `${styles.main} ${styles.main__loading}` : styles.main;
+  const closeInfo = () => {
+    setSearchParams(`frontpage=${frontPage}`);
+  };
 
+  const onRequest = async (value: string, page: string) => {
+    getAllCharacters(value, page).then((res) => {
+      setCharList(res.results);
+      setTotalCount(res.count);
+      setProcess('confirmed');
+    });
+    closeInfo();
+  };
+
+  const handleSubmit = async () => {
+    onRequest(inputValue, '1');
+    setSearchParams('frontpage=1');
+  };
+
+  const onPageChange = (value: number) => {
+    setSearchParams(`frontpage=${value}`);
+  };
+
+  useEffect(() => {
+    const totalPageCount = Math.ceil(totalCount / 10);
+    if (inputValue !== '' && totalPageCount < Number(frontPage)) {
+      onRequest(inputValue, '1');
+      setSearchParams('frontpage=1');
+    } else {
+      onRequest(inputValue, frontPage);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [frontPage]);
+
+  const elements = useMemo(
+    () =>
+      setContent(
+        process,
+        <>
+          <ErrorBoundary>
+            <div className={styles.leftColumn}>
+              <SearchInput value={inputValue} onSearchChange={handleSearchInputChange} handleSubmit={handleSubmit} />
+              <CharList data={charList} openInfo={openInfo} />
+              <Pagination totalCount={totalCount} currentPage={Number(frontPage)} onPageChange={onPageChange} />
+              {charID && <div className={styles.backdrop} onClick={() => closeInfo()} role="button" tabIndex={0} />}
+            </div>
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
+        </>,
+      ),
+    // eslint-disable-next-line
+    [process, inputValue, charID],
+  );
+
+  const wrapperClass = charID ? `${styles.wrapper} ${styles.info}` : styles.wrapper;
   return (
-    <main className={mainClass}>
-      <div className={styles.wrapper}>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <div className={styles.container}>
-            <Search value={inputValue} onSearchChange={handleSearchInputChange} handleSubmit={handleSubmit} />
-            <ErrorBoundary>
-              <CharList data={charList} />
-            </ErrorBoundary>
-          </div>
-        )}
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={wrapperClass}>{elements}</div>
       </div>
     </main>
   );
