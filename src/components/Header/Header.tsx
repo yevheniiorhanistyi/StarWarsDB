e0b6b647b75fe3e6a1abdaa5e4d931ae98aa5742
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-02 13:41:18 +0100

    feat: move images in public folder

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,21 @@
 import Nav from '../Nav/Nav';
-import logo from '../../assets/logos/star_wars.png';
 
 import styles from './Header.module.scss';
 
-const Header: React.FC = () => (
-  <header className={styles.header}>
-    <div className={styles.container}>
-      <div className={styles.outerBox}>
-        <a href="/">
-          <img className={styles.header__logo} src={logo} alt="Star Wars" />
-        </a>
-        <Nav />
+const Header: React.FC = () => {
+  const logo = '/logos/star_wars.png';
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.outerBox}>
+          <a href="/">
+            <img className={styles.header__logo} src={logo} alt="Star Wars" />
+          </a>
+          <Nav />
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
