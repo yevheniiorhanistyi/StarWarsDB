70a18332b292b0449191d6dbf399d908c360f37a
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-02 13:48:42 +0100

    feat: add initialCharData constant

---
+++ src/constats/constats.ts
@@ -0,0 +1,22 @@
+import { ICharData } from '../types/types';
+
+export const initialCharData: ICharData = {
+  name: '',
+  height: '',
+  mass: '',
+  hair_color: '',
+  skin_color: '',
+  eye_color: '',
+  birth_year: '',
+  gender: '',
+  homeworld: '',
+  films: [],
+  species: [],
+  vehicles: [],
+  starships: [],
+  created: new Date(),
+  edited: new Date(),
+  url: '/api/people/1/',
+};
+
+export default initialCharData;
