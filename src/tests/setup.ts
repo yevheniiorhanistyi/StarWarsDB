ee1c106bd7b2a30e1f3255c827f5ae055c709759
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-11 14:23:26 +0100

    chore: install msw and add configure files

---
+++ src/tests/setup.ts
@@ -0,0 +1,13 @@
+import { beforeAll, afterEach, afterAll } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import { server } from '../mocks/server';
+import '@testing-library/jest-dom/vitest';
+
+beforeAll(() => server.listen());
+
+afterEach(() => {
+  cleanup();
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
