e46d4480bf7f3dc61bb1e48d1b4cf55e5d223443
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-18 16:30:24 +0100

    feat: add api config

--- src/services/swApi.ts
+++ src/services/swApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { ICharData, QueryParams, IResourceResponse } from '../types/types';
+import type { ICharData, IResourceResponse, QueryParams } from '../types/types';
 
 export const swApi = createApi({
   reducerPath: 'swApi',
