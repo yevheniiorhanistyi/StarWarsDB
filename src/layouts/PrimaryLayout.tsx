22d0a6564d4890da128f7e8bde247673da6f8cd2
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-29 18:31:36 +0100

    feat: add main wrapper

--- src/layouts/PrimaryLayout.tsx
+++ src/layouts/PrimaryLayout.tsx
@@ -3,10 +3,16 @@ import { Outlet } from 'react-router';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
+import styles from './PrimaryLayout.module.scss';
+
 const PrimaryLayout: React.FC = () => (
   <>
     <Header />
-    <Outlet />
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Outlet />
+      </div>
+    </main>
     <Footer />
   </>
 );
