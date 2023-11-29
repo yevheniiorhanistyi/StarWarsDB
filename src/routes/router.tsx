c4027baba46c44518da9ca4b9ab9c0d147b753a2
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-11-29 18:30:50 +0100

    feat: add People page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import PrimaryLayout from '../layouts/PrimaryLayout';
 import Main from '../pages/Main/Main';
+import People from '../pages/People/People';
 import AdditionalInfo from '../components/AdditionalInfo/AdditionalInfo';
 import NotFound404 from '../pages/NotFound404/NotFound404';
 
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Main />,
+      },
+      {
+        path: '/people',
+        element: <People />,
         children: [
           {
             path: '',
